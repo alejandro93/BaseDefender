a0fc1fd3c04c561bcde1436b0918b47af0ce49bb
alejandro93 <alejandrobp93@gmail.com>
2015-04-08 21:00:46 +0200

    First commit

---
+++ Assets/Scripts/JineteMov.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/** 
+ * Script que determinara el movimiento de la unidad enemiga 
+ * 
+ * @Author: Alejandro Benadero Peris
+ * 
+ * */
+public class JineteMov : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		float X = 0.05f;
+		float Y = 0f;
+
+		//Asignar velocidad en fuerza fija
+		//rigidbody2D.AddForce (new Vector2 (10, 5));
+		transform.position = new Vector2(transform.position.x + X, transform.position.y);
+
+		//Cambiar la direccion a donde mira el sprite
+		transform.localScale = new Vector2 (1, 1);
+
+	}
+}
---
+++ Assets/Scripts/JineteMov.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var pointB : Vector2;
+
+function Start () {
+	var pointA = transform.position;
+	while (true) 
+	{
+		yield MoveObject(transform, pointA, pointB, 3.0);
+		yield MoveObject(transform, pointB, pointA, 3.0); 
+	}
+}
+function MoveObject (thisTransform : Transform, startPos : Vector2, endPos : Vector2, time : float) {
+	var i = 0.0;
+	var rate = 1.0/time;
+	while (i < 1.0) {
+		i += Time.deltaTime * rate;
+		thisTransform.position = Vector2.Lerp(startPos, endPos, i);
+		yield;
+	}
+}
+
+function Update () {
+
+}
