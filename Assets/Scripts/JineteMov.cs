45d351176ab042f18a6e437c3c4e78f46c4b1b40
alejandro93 <alejandrobp93@gmail.com>
2015-04-20 18:34:55 +0200

    Desarrollo

--- Assets/Scripts/JineteMov.cs
+++ Assets/Scripts/JineteMov.cs
@@ -9,17 +9,63 @@ using System.Collections;
  * */
 public class JineteMov : MonoBehaviour {
 
+	public float X = 0.05f;
+	public float Y = 0f;
+	public bool hit = false;
+
+	public Animator animator;
+	//private Movement movement;
+
+	void Start() {
+		//movement.GetComponent<Movement> ();
+		animator.GetComponent<Animator> ();
+	}
+
 	// Update is called once per frame
 	void Update () {
-		float X = 0.05f;
-		float Y = 0f;
+
+		transform.position = new Vector2 (transform.position.x + X, transform.position.y);
 
 		//Asignar velocidad en fuerza fija
 		//rigidbody2D.AddForce (new Vector2 (10, 5));
-		transform.position = new Vector2(transform.position.x + X, transform.position.y);
+
+		if (hit == false) {
+			X = 0.02f;
+			Y = 0f;
+			animator.SetInteger("AnimState", 0);
+
+		} else {
+			X = 0f;
+			Y = 0f;
+			animator.SetInteger("AnimState", 1);
+		}
 
 		//Cambiar la direccion a donde mira el sprite
 		transform.localScale = new Vector2 (1, 1);
 
 	}
+	
+
+	void OnTriggerEnter2D (Collider2D target) {
+		if (target.gameObject.tag == "Ally") {
+			hit = true;
+		}
+		if (target.gameObject.tag == "Enemy") {
+			hit = false;
+		}
+
+	}
+	void OnTriggerExit2D () {
+		hit = false;
+	}
+	void OnTriggerStay2D (Collider2D target){
+		if (target.gameObject.tag == "Ally") {
+			hit = true;
+		}
+		if (target.gameObject.tag == "Enemy") {
+			hit = false;
+		}
+	}
+
+
 }
